5269ea49ca655b0f0cd945a479cf54d227bed7a9
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-09-20 12:05:13 -0300

    add config i project

---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,10 @@
+export * from "./OrthographyPage";
+export * from "./ProsConsPage";
+export * from "./ProsConsStreamPage";
+export * from "./TranslatePage";
+export * from "./AudioToTextPage";
+export * from "./TextToAudioPage";
+export * from "./ImageGenerationPage";
+export * from "./ImageTunningPage";
+export * from "./AssistantPage";
+
